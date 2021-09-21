2561b3f13ca22eb4be22cf9a1b617a22030e121d
moh <moh10moh78@gmail.com>
2021-09-21 07:58:07 -0700

    added the extensions for widgets that we need for now

--- lib/easy_ui.dart
+++ lib/easy_ui.dart
@@ -4,34 +4,106 @@ import 'package:flutter/material.dart';
 
 /// extension of the [num] to contain more useful helper methods and getters
 extension EasyUINumberExtension on num {
-// START Padding helpers
   /// returns an [EdgeInsets] that includes this number as a padding for all sides
   EdgeInsets get allInset {
     return EdgeInsets.all(this.toDouble());
   }
-
   /// returns an [EdgeInsets] that uses [num] as for horizontal insets
   EdgeInsets get horizontalInset => EdgeInsets.symmetric(horizontal: this.toDouble());
-
   /// returns an [EdgeInsets] that uses [num] as for vertical insets
   EdgeInsets get verticalInset => EdgeInsets.symmetric(vertical: this.toDouble());
-
   EdgeInsets get bottomInset => EdgeInsets.only(bottom: this.toDouble());
   EdgeInsets get rightInset => EdgeInsets.only(right: this.toDouble());
   EdgeInsets get leftInset => EdgeInsets.only(left: this.toDouble());
   EdgeInsets get topInset => EdgeInsets.only(top: this.toDouble());
 
-// End Padding helpers
-
-// START Widget helpers
   /// creates a [Widget] that has this [num] as its height
   Widget get heightBox => SizedBox(height: this.toDouble());
-
   /// creates a [SizedBox] that has this [num] as its width
   Widget get widthBox => SizedBox(width: this.toDouble());
-
   /// creates an [Expanded] Widget with [SizedBox] as its child and has this [num] as it's flex
   Widget get expanded => Expanded(flex: this.toInt(), child: const SizedBox());
-// END Widget helpers
+}
 
+extension EasyUIWidgetExtension on Widget {
+
+  /// Makes this widget have round corners. You could also specify [border]
+  Widget withRoundCorners({
+    BoxBorder? border,
+    BorderRadius? borderRadius = const BorderRadius.all(Radius.circular(14))
+  }) {
+    return ClipRRect(
+      borderRadius: borderRadius,
+      child: this,
+    );
+  }
+
+  /// Wrap this widget with a Expanded Widget
+  Expanded get expanded {
+    return Expanded(child: this);
+  }
+
+  /// Wrap this widget with a Expanded Widget and specify a [flex] for it
+  Expanded expandedWith(int flex) {
+    return Expanded(flex: flex,child: this);
+  }
+
+  /// Wrap this widget with a padding for a given [EdgeInsetsGeometry]
+  Widget withPadding(EdgeInsetsGeometry padding) {
+    return Padding(padding: padding, child: this);
+  }
+
+  /// Set the height of this widget set to [height] by wrapping it inside a [SizedBox]
+  Widget withHeight(double? height) {
+    return SizedBox(height: height, child: this);
+  }
+
+  /// Set the min height of this widget to [minHeight] by wrapping it inside a [ConstrainedBox]
+  Widget withMinHeight(double minHeight) {
+    return ConstrainedBox(constraints:BoxConstraints(minHeight: minHeight), child: this);
+  }
+
+  /// Set the width of this widget set to [width] by wrapping it inside a [SizedBox]
+  Widget withWidth(double? width) {
+    return SizedBox(width: width, child: this);
+  }
+
+  /// Set the min width of this widget to [minWidth] by wrapping it inside a [ConstrainedBox]
+  Widget withMinWidth(double minWidth) {
+    return ConstrainedBox(constraints:BoxConstraints(minWidth: minWidth), child: this);
+  }
+
+  /// Wrap this widget with a [ConstrainedBox], and set it's constraints to [constraints]
+  Widget withConstraints(BoxConstraints constraints) {
+    return ConstrainedBox(constraints: constraints, child: this);
+  }
+
+  /// The number of clockwise quarter turns the child should be rotated.
+  Widget rotate(int quarterTurns) {
+    return RotatedBox(quarterTurns: quarterTurns, child: this);
+  }
+}
+
+/// Extensions about aligning widgets on screen
+extension EasyUIAlignWidgetExtension on Widget {
+  Align align(AlignmentGeometry alignmentGeometry) => Align(alignment: alignmentGeometry, child: this);
+  Align get alignCenter => Align(alignment: Alignment.center, child: this);
+  Align get alignBottom => Align(alignment: Alignment.bottomCenter, child: this);
+  Align get alignBottomLeft => Align(alignment: Alignment.bottomLeft, child: this);
+  Align get alignBottomRight => Align(alignment: Alignment.bottomRight, child: this);
+  Align get alignRight => Align(alignment: Alignment.centerRight, child: this);
+  Align get alignTop => Align(alignment: Alignment.topCenter, child: this);
+  Align get alignTopLeft => Align(alignment: Alignment.topLeft, child: this);
+  Align get alignTopRight => Align(alignment: Alignment.topRight, child: this);
+  Align get alignLeft => Align(alignment: Alignment.centerLeft, child: this);
+}
+
+/// Extensions about fitting widgets
+extension EasyUIFitWidgetExtension on Widget {
+  Widget get fit => FittedBox(child: this);
+  Widget get fitWidth => FittedBox(fit: BoxFit.fitWidth, child: this);
+  Widget get fitHeight => FittedBox(fit: BoxFit.fitHeight, child: this);
+  Widget get fitCover => FittedBox(fit: BoxFit.cover, child: this);
+  Widget get fitFill => FittedBox(fit: BoxFit.fill, child: this);
+  Widget get fitScaleDown => FittedBox(fit: BoxFit.scaleDown, child: this);
 }
