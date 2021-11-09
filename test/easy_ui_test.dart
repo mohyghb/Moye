292c61a8be0af90b8e0a902963a9afd09c2f7b4b
moh <moh10moh78@gmail.com>
2021-11-08 22:00:00 -0800

    changed the package name to moye

--- test/easy_ui_test.dart
+++ test/easy_ui_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:easy_ui/easy_ui.dart';
+import 'package:moye/easy_ui.dart';
 
 void main() {
   // test('adds one to input values', () {
