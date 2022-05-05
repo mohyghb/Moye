1a8ff40001e64b7e8596f8c96ea254a6c9af8efc
moh <moh10moh78@gmail.com>
2022-05-05 12:26:26 -0700

    exported constants inside the moye file

--- lib/moye.dart
+++ lib/moye.dart
@@ -1,5 +1,6 @@
 /// This file exports all the functionality of this package
 export 'package:moye/extensions/moye_extensions.dart';
+export 'package:moye/constants.dart';
 export 'package:moye/extensions/datetime_extension.dart';
 export 'package:moye/utils/easy_ui_container.dart';
 export 'package:moye/utils/easy_ui_text_field.dart';
