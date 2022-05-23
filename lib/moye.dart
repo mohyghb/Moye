2e41133f2ece24c364e584ff5eaa86ab06498026
moh <moh10moh78@gmail.com>
2022-05-23 10:30:40 -0700

    added title case and capitalize for string extensions

--- lib/moye.dart
+++ lib/moye.dart
@@ -1,6 +1,7 @@
 /// This file exports all the functionality of this package
 export 'package:moye/extensions/moye_extensions.dart';
 export 'package:moye/extensions/datetime_extension.dart';
+export 'package:moye/extensions/string_extensions.dart';
 export 'package:moye/utils/easy_ui_container.dart';
 export 'package:moye/utils/easy_ui_text_field.dart';
 export 'package:moye/widgets/auto_layout.dart';
